cbd57e799b806319035bc0c9fd64f5e69fe385f8
pixochi <kozak.jakub55@gmail.com>
2023-01-04 18:22:33 +0100

    Updates

---
+++ precache-manifest.a2aa3381ca6095a818a5e39b412f9d7a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0d3f3de90a6fa7dfee1142fc12a2d391",
+    "url": "/socket.io-react-hooks-chat-client-v2/index.html"
+  },
+  {
+    "revision": "5350224e9eb6957ecca6",
+    "url": "/socket.io-react-hooks-chat-client-v2/static/css/main.910aa548.chunk.css"
+  },
+  {
+    "revision": "6e560c2f31c05764feb1",
+    "url": "/socket.io-react-hooks-chat-client-v2/static/js/2.a961b84d.chunk.js"
+  },
+  {
+    "revision": "0b08544f54c9906705be39c03b7e79ad",
+    "url": "/socket.io-react-hooks-chat-client-v2/static/js/2.a961b84d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5350224e9eb6957ecca6",
+    "url": "/socket.io-react-hooks-chat-client-v2/static/js/main.da08c1a8.chunk.js"
+  },
+  {
+    "revision": "01113fcf0e35f4246f8c",
+    "url": "/socket.io-react-hooks-chat-client-v2/static/js/runtime-main.f28747b9.js"
+  }
+]);
